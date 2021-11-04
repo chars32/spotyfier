f55c42030dd730f6b95ec9ce77d01feaa73a85c8
charsDev <chars24@gmail.com>
2021-11-03 22:00:45 -0600

    Login or Register page

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AuthOptions from '../../components/Auth/AuthOptions'
+import LoginForm from '../../components/Auth/LoginForm'
+import RegisterForm from '../../components/Auth/RegisterForm'
+import BackgroundApp from '../../assets/jpg/background-auth.jpg'
+import LogoNameWhite from '../../assets/png/logo-name-white.png'
+
+import './Auth.scss'
 
 export default function Auth() {
+  const [selectedForm, setSelectForm] = useState(null)
+
+  const handlerForm = () => {
+    switch (selectedForm) {
+      case 'login':
+        return <LoginForm />
+      case "register":
+        return <RegisterForm />
+      default:
+        return <AuthOptions />
+    }
+  }
+
   return (
-    <div>
-      <h2>Iniciar Sesión</h2>
-      <h2>Registrarse</h2>
+    <div className='auth' style={{ backgroundImage: `url(${BackgroundApp})` }}>
+      <div className='auth__dark' />
+      <div className='auth__box'>
+        <div className='auth__box-logo'>
+          <img src={LogoNameWhite} alt='Soptyfier ' />
+        </div>
+        {handlerForm()}
+      </div>
     </div>
   )
 }
