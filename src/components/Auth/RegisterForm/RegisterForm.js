1a571c5e326817b33c07475f1737d8d94d05721c
charsDev <chars24@gmail.com>
2021-11-08 23:16:15 -0600

    Creating User's form register

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,9 +1,63 @@
 import React from 'react'
 
-export default function RegisterForm() {
+import { Button, Icon, Form, Input } from 'semantic-ui-react'
+// import app from '../../../utils/Firebase'
+// import { getAuth } from 'firebase/auth'
+
+import './RegisterForm.scss'
+
+export default function RegisterForm({ setSelectedForm }) {
+
+  const onSubmit = () => {
+    console.log('Formulario enviado')
+  }
+
   return (
-    <div>
-      <h1>RegisterForm...</h1>
+    <div className='register-form'>
+      <h1>Empieza a escuchar con una cuenta de Spotyfier gratis</h1>
+      <Form onSubmit={onSubmit}>
+        <Form.Field>
+          <Input
+            type='text'
+            name='email'
+            placeholder='Correo electronico'
+            icon='mail outline'
+          // onChange={}
+          // error={}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Input
+            type='password'
+            name='password'
+            placeholder='Contraseña'
+            icon='eye'
+          // onChange={}
+          // error={}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Input
+            type='text'
+            name='username'
+            placeholder='Como deberiamos llamarte'
+            icon='user circle outline'
+          // onChange={}
+          // error={}
+          />
+        </Form.Field>
+        <Button type='submit'>
+          Continuar
+        </Button>
+      </Form>
+
+      <div className='register-form__options'>
+        <p onClick={() => setSelectedForm(null)}>Volver</p>
+        <p>
+          ¿Ya tienes Spotyfier?{" "}
+          <span onClick={() => setSelectedForm('login')}>Iniciar Sesión</span>
+        </p>
+      </div>
     </div>
   )
 }
